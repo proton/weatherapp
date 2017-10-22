630e58dab34a4c9ef25ec4b44210184fc3fdadce
Peter Savichev (proton) <psavichev@gmail.com>
2017-10-22 20:09:12 +0300

    locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first || I18n.default_locale
+  end
 end
